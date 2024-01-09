eb5012e8ad5d3c7783dc19674fdd3ade299f0129
abdullahshav <144883374+abdullahshav@users.noreply.github.com>
2024-01-09 16:05:57 +0500

    Update Generating Sales Ranking Report.sql

--- Generating Sales Ranking Report.sql
+++ Generating Sales Ranking Report.sql
@@ -1,30 +1,14 @@
-SELECT
-    c.customer_id,
-    c.customer_name,
-    s.sales_channel,
-    ROUND(SUM(s.total_sales), 2) AS total_sales
-FROM
-    customers c
-JOIN
-    sales s ON c.customer_id = s.customer_id
-WHERE
-    c.customer_id IN (
-        SELECT
-            customer_id
-        FROM
-            (
-                SELECT
-                    customer_id,
-                    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY total_sales DESC) AS sales_rank
-                FROM
-                    sales
-                WHERE
-                    sales_year IN (1998, 1999, 2001)
-            ) ranked_sales
-        WHERE
-            sales_rank <= 300
-    )
-GROUP BY
-    c.customer_id, c.customer_name, s.sales_channel
-ORDER BY
-    s.sales_channel, total_sales DESC;
+WITH RankedCustomers AS (
+ SELECT cust_id, cust_first_name, cust_last_name, channel_desc, calendar_year,
+	    ROUND(SUM(amount_sold), 2) AS total_sales,
+        RANK() OVER (PARTITION BY calendar_year, channel_desc ORDER BY SUM(amount_sold) DESC) AS sales_rank
+ FROM sh.customers cust
+ JOIN sh.sales s USING(cust_id)
+ JOIN sh.channels ch USING(channel_id)
+ JOIN sh.times t USING(time_id)
+ WHERE t.calendar_year IN (1998, 1999, 2001)
+ GROUP BY cust_id, channel_desc, calendar_year
+)
+SELECT cust_id, cust_first_name, cust_last_name, channel_desc, calendar_year, total_sales, sales_rank 
+FROM RankedCustomers
+WHERE sales_rank <= 300;
